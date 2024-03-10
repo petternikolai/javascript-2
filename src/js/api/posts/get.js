ffbe0001ecad56780d2d49b20695b106481e7d31
Petter-Nikolai Kristoffersen <pnkristoffersen@gmail.com>
2024-03-10 15:24:33 +0100

    Bug fixes

--- src/js/api/posts/get.js
+++ src/js/api/posts/get.js
@@ -8,16 +8,11 @@ import { authFetch } from "../fetch.js";
 
 /**
  * Retrieves a list of posts from the server.
- * @param {number} [page=1] - The page number of posts to retrieve.
- * @param {number} [limit] - The maximum number of posts to retrieve per page.
  * @returns {Promise<object>} A promise that resolves to the response data containing the posts.
  * @throws {Error} If the request fails or an error occurs during retrieval.
  */
-export async function getPosts(page = 1, limit) {
-  let url = `${API_BASE}${API_POSTS}?page=${page}&${API_QUERY_AUTHOR}&${API_QUERY_COMMENT}`;
-  if (limit !== undefined) {
-    url += `&limit=${limit}`;
-  }
+export async function getPosts() {
+  let url = `${API_BASE}${API_POSTS}?${API_QUERY_AUTHOR}&${API_QUERY_COMMENT}`;
   const response = await authFetch(url);
   return await response.json();
 }
